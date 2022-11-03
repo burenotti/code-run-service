a9ca090d8e5c013e353791800629d79e9d1d777f
Artem <burenotti@gmail.com>
2022-11-03 13:51:29 +0300

    🎉 Initial commit

--- service/settings.py
+++ service/settings.py
@@ -1,15 +1,30 @@
+from pathlib import Path
+from typing import Mapping
+
 from pydantic import BaseSettings, BaseModel
+
+
+class RunboxSettings(BaseModel):
+    docker_url: str | None = None
+    cfg_dir: Path = './pipelines'
+
+    stages: Mapping[str, str] = {
+        "use_sandbox": "runbox.build_stages.stages:UseSandbox",
+        "use_volume": "runbox.build_stages.stages:UseVolume",
+        "write_files": "service.stages:WriteFiles"
+    }
 
 
 class UvicornSetting(BaseModel):
     host: str = '127.0.0.1'
-    port: int = '8080'
+    port: int = 8080
     workers: int = 1
     reload: bool = True
 
 
 class Settings(BaseSettings):
-    uvicorn: UvicornSetting
+    uvicorn: UvicornSetting = UvicornSetting()
+    runbox: RunboxSettings = RunboxSettings()
 
 
 settings = Settings(
