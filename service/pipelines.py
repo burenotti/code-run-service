e2ed8d023efb7d8c9cc328a955330a22c05ccb74
Artem <burenotti@gmail.com>
2022-11-06 15:58:05 +0300

    ♻️ Refactored Execution Service using AsyncBasePipeline

--- service/pipelines.py
+++ service/pipelines.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
 import asyncio
-from dataclasses import dataclass
+import functools
+from dataclasses import dataclass, field
 from itertools import groupby
 from pathlib import Path
-from typing import Type
+from typing import Type, Protocol, Mapping
 
 from pydantic import BaseModel
 from runbox.build_stages import Pipeline, BasePipeline, load_stages, JsonPipelineLoader, PipelineLoader
+from runbox.build_stages.pipeline import AsyncBasePipeline
 
 from service.settings import settings
 
@@ -26,10 +28,26 @@ class LanguageInfo(BaseModel):
     default_version: str | None = None
 
 
+class AppAsyncPipeline(AsyncBasePipeline):
+
+    async def on_finalize(self) -> None:
+        ...
+
+    async def on_group_done(self, group: str) -> None:
+        print(f"Done: {group}")
+
+    async def on_group_failed(self, group: str) -> None:
+        print(f"Failed: {group}")
+
+
 class PipelineFactory:
 
     def __init__(self, pipelines: list[PipelineMeta]):
         self.pipelines = pipelines
+        self._pipeline_class: Type[Pipeline] = AsyncBasePipeline
+
+    def set_pipeline_class(self, class_: Type[Pipeline]) -> None:
+        self._pipeline_class = class_
 
     def get(self, language: str, version: str) -> Pipeline:
         selected: PipelineLoader | None = None
@@ -44,7 +62,7 @@ class PipelineFactory:
         if selected is None:
             raise ValueError(f"Suitable pipeline for {language} {version} not found")
 
-        return selected.load()
+        return selected.fill(self._pipeline_class())
 
     def languages(self) -> list[LanguageInfo]:
         langs = []
@@ -63,20 +81,18 @@ class PipelineFactory:
     def load_pipelines(
             cls,
             dir_path: Path | str,
-            stages_map: dict[str, str] = settings.runbox.stages,
-            class_: Type[Pipeline] = BasePipeline,
+            stages_map: Mapping[str, str] = settings.runbox.stages,
     ) -> "PipelineFactory":
         if not isinstance(dir_path, Path):
             dir_path = Path(dir_path)
 
         pipelines: list[PipelineMeta] = []
-        stages = load_stages(stages_map)
+        stages = load_stages(dict(stages_map))
         for file in dir_path.iterdir():
             if file.suffix == ".json" and file.is_file():
-                loader = JsonPipelineLoader[Pipeline](
+                loader = JsonPipelineLoader(
                     path=file,
                     stage_getter=lambda stage_name: stages[stage_name],
-                    class_=class_,
                 )
                 pipeline = PipelineMeta(
                     language=loader.meta['language'],
